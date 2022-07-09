a7b829a3013a064978ba5a2b8aa13ce0934a5ea7
Izaura <izahonorato@gmail.com>
2022-07-09 15:35:18 -0300

    endpoint login

--- Main.py
+++ Main.py
@@ -1,8 +1,11 @@
 from typing import Optional
 from fastapi import FastAPI
 from pydantic import BaseModel
+from routes.UserRoute import router as UsuarioRouter
 
 app = FastAPI()
+
+app.include_router(UsuarioRouter, tags=["Usuario"], prefix="/api/usuario")
 
 @app.get("/api/health", tags=["Health"])
 async def health():
--- routes/UserRoute.py
+++ routes/UserRoute.py
@@ -3,3 +3,7 @@ from fastapi import APIRouter
 router = APIRouter()
 
 @router.post("/", response_description="Rota para criar um novo usuário.")
+async def rota_criar_usuario(usuario):
+    return{
+        "mensagem": "Usuário cadastrado com sucesso."
+    }
